812f8bdab4089b124743248571a5256e6482ad0c
JosuAlejandroAl <A01732454@itesm.mx>
2024-04-09 16:47:22 -0600

    test 2 pregunta 2

--- clite.py
+++ clite.py
@@ -5,12 +5,10 @@ tokens = ['INT']
 
 t_ignore = ' \t'
 
-
 def t_INT(t):
-    r'\d+'
-    t.value = int(t.value)
+    r'\d+(_\d+)*'
+    t.value = int(t.value.replace('_', ''))
     return t
-
 
 def getLexer():
     return lex.lex()
