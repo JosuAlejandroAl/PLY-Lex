92956bacbb09c902406b809ccb0e1fc38d015714
JosuAlejandroAl <A01732454@itesm.mx>
2024-04-09 23:30:52 -0600

    test de cadenas pregunta 4

--- clite.py
+++ clite.py
@@ -1,7 +1,7 @@
 
 import ply.lex as lex
 
-tokens = ['INT', 'FLOAT']
+tokens = ['INT', 'FLOAT', 'STR']
 
 t_ignore = ' \t'
 
@@ -13,6 +13,10 @@ def t_FLOAT(t):
 def t_INT(t):
     r'\d+(_\d+)*'
     t.value = int(t.value.replace('_', ''))
+    return t
+
+def t_STR(t):
+    r'^[\'"].*[\'"]$'
     return t
 
 def getLexer():
